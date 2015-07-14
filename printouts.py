42b07b7fdc389e0617e1257c8fd2f11cacf05769
Austin Clarke <austie.clarke@gmail.com>
2015-07-14 22:12:08 +1200

    added and tested some things

---
+++ printouts.py
@@ -0,0 +1,71 @@
+#printouts are a item portrait that will be printed on item pickups or equips.
+def longsword_printout():
+    print(" /\ ")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print(" ||")
+    print("(==)")
+    print(" []")
+    print(" []")
+    print(" ()")
+
+def shortsword_printout():
+     print(" /|")
+     print(" ||")
+     print(" ||")
+     print(" ||")
+     print("[==]")
+     print(" []")
+     print(" <>")
+
+def dagger_printout():
+     print(" /|")
+     print(" ||")
+     print(" ||")
+     print("<++>")
+     print(" []")
+     print(" ()")
+
+def club_printout():
+    print("{  }")
+    print("{  }")
+    print("{  }")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+
+def wooden_staff_printout():
+    print("\<>/")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" []")
+    print(" ()")
+
+def axe_printout():
+    print(" /  ||  \ ")
+    print("|   ||   |")
+    print(" \  ||  /")
+    print("    []")
+    print("    []")
+    print("    []")
+    print("    []")
+    print("    []")
+
+
+#club_printout()
+#shortsword_printout()
+#wooden_staff_printout()
+#axe_printout()
+#dagger_printout()
+#longsword_printout()
